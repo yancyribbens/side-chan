478058db601012ee5c6eb063ca4bb83bb258724e
yancy <yancy@yancy.lol>
2022-11-29 18:13:30 +0100

    Initial commit

---
+++ side_chan.rs
@@ -0,0 +1,26 @@
+// inspired by https://electricdusk.com/cmov-conversion.html
+
+// Select an element from an array in constant time
+pub fn constant_time_lookup(secret_idx: usize,
+                            table: [u64; 16]) -> bool {
+    let mut result: i64 = 0;
+
+    for (idx, value) in table.iter().enumerate() {
+        let cond = idx == secret_idx;
+        let mask = cond as i64;
+        result |= (*value as i64) & mask;
+    }
+    result != 0
+}
+
+fn main() {
+	let table: [u64; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+	let secret_idx = 99;
+	let result = constant_time_lookup(secret_idx, table);
+	assert_eq!(false, result);
+
+	let secret_idx = 11;
+	let result = constant_time_lookup(secret_idx, table);
+	assert_eq!(true, result);
+}
