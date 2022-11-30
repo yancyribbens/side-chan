a6edd42d2e5c0d8bb634a57079ee3b8179130e12
yancy <yancy@yancy.lol>
2022-11-30 14:29:25 +0100

    Add timing

--- side_chan.rs
+++ side_chan.rs
@@ -1,4 +1,5 @@
 // inspired by https://electricdusk.com/cmov-conversion.html
+use std::time;
 
 // Select an element from an array in non-constant time
 pub fn non_constant_time_lookup(secret_idx: u64,
@@ -28,11 +29,63 @@ pub fn constant_time_lookup(secret_idx: usize,
 fn main() {
 	let table: [u64; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-	let secret_idx = 99;
-	let result = constant_time_lookup(secret_idx, table);
-	assert_eq!(false, result);
+    // constant time
+    println!("constant time:");
+    let secret_idx = 99;
+    let now = time::Instant::now();
+    let result = constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - not found: {:?}", elapsed_time);
+    assert_eq!(false, result);
 
-	let secret_idx = 11;
+	let secret_idx = 0;
+    let now = time::Instant::now();
 	let result = constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
+	assert_eq!(true, result);
+
+	let secret_idx = 8;
+    let now = time::Instant::now();
+	let result = constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
+	assert_eq!(true, result);
+
+	let secret_idx = 15;
+    let now = time::Instant::now();
+	let result = constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
+	assert_eq!(true, result);
+
+    // non-constant time
+    println!("non-constant time:");
+    let secret_idx = 99;
+    let now = time::Instant::now();
+    let result = non_constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - not found: {:?}", elapsed_time);
+    assert_eq!(false, result);
+
+	let secret_idx = 0;
+    let now = time::Instant::now();
+	let result = non_constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
+	assert_eq!(true, result);
+
+	let secret_idx = 8;
+    let now = time::Instant::now();
+	let result = non_constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
+	assert_eq!(true, result);
+
+	let secret_idx = 15;
+    let now = time::Instant::now();
+	let result = non_constant_time_lookup(secret_idx, table);
+    let elapsed_time = now.elapsed();
+    println!("ellapsed - found: {:?}", elapsed_time);
 	assert_eq!(true, result);
 }
